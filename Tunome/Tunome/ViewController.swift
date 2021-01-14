6db1252c3070d67eb44099cb7f9453ff7f59efae
Jason9789 <lifebook0809@gmail.com>
2021-01-15 00:48:26 +0900

    [ADD] Metronome + Tuner file init

---
+++ Tunome/Tunome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tunome
+//
+//  Created by 전판근 on 2021/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let bpmLabel = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
